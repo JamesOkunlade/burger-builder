7f3c4628f7ac25794b83f69413890399296bdb74
James Okunlade <james.okunlade@gmail.com>
2020-01-09 12:57:22 +0100

    burger state added

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+
+
+class BurgerBuilder extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 0
+    }
+
+    this.handleClick = () => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }
+  }
+    render () {
+      let {count} = this.state;
+
+        return (
+            <Aux>
+                <div>I want { count } Burgers</div>
+                <button onClick={(e) => this.handleClick()}>Add burger</button>
+            </Aux>
+        );
+    }
+}
+
+
+export default BurgerBuilder;
