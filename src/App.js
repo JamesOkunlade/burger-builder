4296f273729b62f0a1641578675fc3f1fc9e3e46
James Okunlade <james.okunlade@gmail.com>
2020-01-15 16:32:23 +0100

    added axios dependency to the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout> 
-    </div>
-  );
+class App extends Component{
+  componentDidMount () {
+    axios.get('localhost:/3000/shipments/')
+      .then(response => {
+        console.log(response);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
